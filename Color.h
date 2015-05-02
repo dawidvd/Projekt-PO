2e48b7b0d0ddda195877f9c8809c4dbcb64cd114
Dawid Hajda <dawhaj@gmail.com>
2015-05-02 17:39:18 +0200

    Dodano kolory z http://flatuicolors.com

--- Color.h
+++ Color.h
@@ -6,14 +6,14 @@
 
 #ifndef SDL_DOMO_Color_h
 #define SDL_DOMO_Color_h
+#include <SDL2/SDL.h>
 struct Color
 {
-    unsigned short r = 255, g = 255, b = 255, a = 255;
-public:
-    void SetRendererColor(SDL_Renderer* renderer) const
-    {
-        SDL_SetRenderDrawColor(renderer, r, g, b, a);
-    }
+	unsigned short r, g, b, a;
+	public:
+	Color(const unsigned short r = 255, const unsigned short g = 255, const unsigned short b = 255, const unsigned short a = 255);
+	void SetRendererColor(SDL_Renderer* renderer) const;
+	static Color GetFlatColor(unsigned int nr);
 };
 
 #endif
